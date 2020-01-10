a88dbdf77a57359a155c8bbe793641ea0bbe1c17
cpalden <cpalden@ryerson.ca>
2020-01-10 01:25:24 -0500

    Logged messages using logging provider, nlog

--- DotnetcorePoc/Controllers/ErrorController.cs
+++ DotnetcorePoc/Controllers/ErrorController.cs
@@ -13,18 +13,27 @@ namespace DotnetcorePoc.Controllers
     {
         private readonly ILogger<ErrorController> logger;
 
+        /*
+         * Injecting ILogger service to log custom messagessages, warnings and exceptions
+         * Using ILogger service methods, Loginformation, LogWarning, LogError... to log
+         */
         public ErrorController(ILogger<ErrorController> logger)
         {
             this.logger = logger;
-        }
+        } 
 
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, the resource you have requested could not be found.";
+                    // logging 404 error as LogWarning
+                    logger.LogWarning($"404 Error occured. Path = {statusCodeResult.OriginalPath}" +
+                        $" and QueryString = {statusCodeResult.OriginalQueryString}");
                     break;
             }
             return View("NotFound");
@@ -36,10 +45,14 @@ namespace DotnetcorePoc.Controllers
         {
             var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
 
-            ViewBag.ExceptionPath = exceptionDetails.Path;
-            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
-            ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
-
+            //logging exception details using string interpolation
+            logger.LogError($"The path here {exceptionDetails.Path} threw an exceptioin " +
+                $"{exceptionDetails.Error}");
+            /*
+             * ViewBag.ExceptionPath = exceptionDetails.Path;
+             * ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+             * ViewBag.Stacktrace = exceptionDetails.Error.StackTrace;
+            */
             return View();
         }
 
